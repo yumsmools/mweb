13b4394884f21733ce18ffad0bde49e7fe31743c
Imran Moolla <imran.moolla@gmail.com>
2019-08-30 08:59:50 +0200

    Initial checkin

--- src/main/java/com/demo/mweb/LoginController.java
+++ src/main/java/com/demo/mweb/LoginController.java
@@ -1,5 +1,30 @@
 package com.demo.mweb;
 
-public class LoginController {
+import java.net.URI;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping(path = "/login")
+public class LoginController {
+    @PostMapping(path= "/", consumes = "application/json", produces = "text")
+    public String addEmployee(@RequestBody User user)
+    {
+        Integer id = employeeDao.getAllEmployees().getEmployeeList().size() + 1;
+        employee.setId(id);
+         
+        employeeDao.addEmployee(employee);
+         
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                                    .path("/{id}")
+                                    .buildAndExpand(employee.getId())
+                                    .toUri();
+         
+        return ResponseEntity.created(location).build();
+    }
 }
---
+++ src/main/java/com/demo/mweb/MwebApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MwebApplication.class, args);
+	}
+
+}
